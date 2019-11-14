6d1c8c920746bc2c34849cb216e950fbe9e83183
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-11-14 17:11:46 -0300

    fix small script typo

--- 8.4/lfs/scripts/book/3.1-Introduction.sh
+++ 8.4/lfs/scripts/book/3.1-Introduction.sh
@@ -4,6 +4,6 @@ mkdir -v $LFS/toolchain
 
 chmod -v a+wt $LFS/toolchain
 
-wget --input-file=scripts/wget-list --continue --directory-prefix=$LFS/toolchain
+wget --input-file=config/wget-list --continue --directory-prefix=$LFS/toolchain
 
 md5sum -c config/md5sums
